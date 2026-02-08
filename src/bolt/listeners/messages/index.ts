22aab92bf0bac37491a72c65be010cd81d5fe76c
AE <aegrey@users.noreply.github.com>
2026-02-08 13:08:58 -0500

    Update index.ts

--- src/bolt/listeners/messages/index.ts
+++ src/bolt/listeners/messages/index.ts
@@ -2,7 +2,7 @@ import type { App } from "@slack/bolt";
 import { sampleMessageCallback } from "./sample-message.js";
 
 const register = (app: App) => {
-  app.message(/^hello.*/, sampleMessageCallback);
+  app.message(/(?:https?:\/\/)?(www\.)?instagram\.com\/\S+/, sampleMessageCallback);
 };
 
 export default { register };
